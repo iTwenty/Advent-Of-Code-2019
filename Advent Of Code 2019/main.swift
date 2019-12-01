11058af786b6a495096800cb4081659391359e7d
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2019-12-01 18:14:27 +0530

    Initial Commit

---
+++ Advent Of Code 2019/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Advent Of Code 2019
+//
+//  Created by jaydeep on 01/12/19.
+//  Copyright © 2019 jaydeep. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
