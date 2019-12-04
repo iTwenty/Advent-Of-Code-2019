ac371cca1073ed24e4cc835387af8107029ef1cc
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2019-12-04 22:46:26 +0530

    Solution for 03_02

---
+++ Advent Of Code 2019/00/Puzzle00.swift
@@ -0,0 +1,3 @@
+import Foundation
+
+// Test puzzle plz ignore
--- Advent Of Code 2019/03/Puzzle03.swift
+++ Advent Of Code 2019/03/Puzzle03.swift
@@ -54,16 +54,53 @@ import Foundation
 
  What is the Manhattan distance from the central port to the closest intersection?
 
+ --- Part Two ---
+
+ It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
+
+ To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where the sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value from the first time it visits that position when calculating the total value of a specific intersection.
+
+ The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+
+ ...........
+ .+-----+...
+ .|.....|...
+ .|..+--X-+.
+ .|..|..|.|.
+ .|.-X--+.|.
+ .|..|....|.
+ .|.......|.
+ .o-------+.
+ ...........
+
+ In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
+
+ However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+
+ Here are the best steps for the extra examples from above:
+
+     R75,D30,R83,U83,L12,D49,R71,U7,L72
+     U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+     R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+     U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+
+ What is the fewest combined steps the wires must take to reach an intersection?
+
  */
 
-fileprivate struct Point: Hashable, Comparable, CustomStringConvertible {
+fileprivate struct Point: Hashable, Equatable {
     let x: Int
     let y: Int
     var manhattanDistance: Int { x + y }
-    var description: String { "\(manhattanDistance)" }
+}
 
-    static func < (lhs: Point, rhs: Point) -> Bool {
-        lhs.manhattanDistance < rhs.manhattanDistance
+fileprivate struct VisitedPoint: Hashable, Equatable {
+    let point: Point
+    let stepsTakenToVisit: Int
+
+    init(x: Int, y: Int, steps: Int) {
+        self.point = Point(x: x, y: y)
+        self.stepsTakenToVisit = steps
     }
 }
 
@@ -86,35 +123,41 @@ struct Puzzle03: Puzzle {
         }
     }
     
-    private func points(forWire wire: [Move]) -> Set<Point> {
-        var points: Set<Point> = []
-        var currentPoint = Point(x: 0, y: 0)
+    private func points(forWire wire: [Move]) -> Set<VisitedPoint> {
+        var points = Set<VisitedPoint>()
+        var steps = 0
+        var currentX = 0
+        var currentY = 0
         wire.forEach { (move) in
             switch move.direction {
             case "R":
-                let newX = currentPoint.x + move.distance
-                ((currentPoint.x + 1)...newX).forEach { (x) in
-                    points.insert(Point(x: x, y: currentPoint.y))
+                let newX = currentX + move.distance
+                ((currentX + 1)...newX).forEach { (x) in
+                    steps = steps + 1
+                    points.insert(VisitedPoint(x: x, y: currentY, steps: steps))
                 }
-                currentPoint = Point(x: newX, y: currentPoint.y)
+                currentX = newX
             case "L":
-                let newX = currentPoint.x - move.distance
-                (newX...(currentPoint.x - 1)).forEach { (x) in
-                    points.insert(Point(x: x, y: currentPoint.y))
+                let newX = currentX - move.distance
+                (newX...(currentX)).forEach { (x) in
+                    steps = steps + 1
+                    points.insert(VisitedPoint(x: x, y: currentY, steps: steps))
                 }
-                currentPoint = Point(x: newX, y: currentPoint.y)
+                currentX = newX
             case "U":
-                let newY = currentPoint.y + move.distance
-                ((currentPoint.y + 1)...newY).forEach { (y) in
-                    points.insert(Point(x: currentPoint.x, y: y))
+                let newY = currentY + move.distance
+                ((currentY + 1)...newY).forEach { (y) in
+                    steps = steps + 1
+                    points.insert(VisitedPoint(x: currentX, y: y, steps: steps))
                 }
-                currentPoint = Point(x: currentPoint.x, y: newY)
+                currentY = newY
             case "D":
-                let newY = currentPoint.y - move.distance
-                (newY...(currentPoint.y - 1)).forEach { (y) in
-                    points.insert(Point(x: currentPoint.x, y: y))
+                let newY = currentY - move.distance
+                (newY...(currentY - 1)).forEach { (y) in
+                    steps = steps + 1
+                    points.insert(VisitedPoint(x: currentX, y: y, steps: steps))
                 }
-                currentPoint = Point(x: currentPoint.x, y: newY)
+                currentY = newY
             default:
                 fatalError("Invalid direction - \(move.direction)")
             }
@@ -123,12 +166,37 @@ struct Puzzle03: Puzzle {
     }
     
     func part1() -> String {
-        let firstWirePoints: Set<Point> = points(forWire: input[0])
-        let secondWirePoints: Set<Point> = points(forWire: input[1])
-        return "\(firstWirePoints.intersection(secondWirePoints).min(by: <)!)"
+        let firstWirePoints = Set(points(forWire: input[0]).map { $0.point })
+        let secondWirePoints = Set(points(forWire: input[1]).map { $0.point })
+        let minManhattanDistance = firstWirePoints.intersection(secondWirePoints).min(by: {
+            $0.manhattanDistance < $1.manhattanDistance
+        })!
+        return "\(minManhattanDistance)"
+        
     }
-    
+
+    // Incorrect answer. Can't figure out what's wrong :(
     func part2() -> String {
-        return ""
+        let firstWireVisitedPoints = points(forWire: input[0])
+        let secondWireVisitedPoints = points(forWire: input[1])
+        let firstWirePoints = Set(firstWireVisitedPoints.map { $0.point })
+        let secondWirePoints = Set(secondWireVisitedPoints.map { $0.point })
+        let intersection = firstWirePoints.intersection(secondWirePoints)
+
+        var minimum = Int.max
+        for intersectionPoint in intersection {
+        let intersectionPoinVisitsByFirstWire = firstWireVisitedPoints.filter { $0.point  == intersectionPoint }
+            let intersectionPoinVisitsBySecondWire = secondWireVisitedPoints.filter { $0.point  == intersectionPoint }
+            for i in intersectionPoinVisitsByFirstWire {
+                for j in intersectionPoinVisitsBySecondWire {
+                    let currentStepSum = (i.stepsTakenToVisit + j.stepsTakenToVisit)
+                    if currentStepSum < minimum {
+                        minimum = currentStepSum
+                    }
+                }
+            }
+        }
+        
+        return "\(minimum)"
     }
 }
