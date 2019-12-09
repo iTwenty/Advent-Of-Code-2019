6b90712223a7a19bca02542f3aa6a75754893004
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2019-12-09 12:23:31 +0530

    Solution for 09_02

--- Advent Of Code 2019/09/Puzzle09.swift
+++ Advent Of Code 2019/09/Puzzle09.swift
@@ -46,6 +46,16 @@
 
  Once your Intcode computer is fully functional, the BOOST program should report no malfunctioning opcodes when run in test mode; it should only output a single value, the BOOST keycode. What BOOST keycode does it produce?
 
+ --- Part Two ---
+
+ You now have a complete Intcode computer.
+
+ Finally, you can lock on to the Ceres distress signal! You just need to boost your sensors using the BOOST program.
+
+ The program runs in sensor boost mode by providing the input instruction the value 2. Once run, it will boost the sensors automatically, but it might take a few seconds to complete the operation on slower hardware. In sensor boost mode, the program will output a single value: the coordinates of the distress signal.
+
+ Run the BOOST program in sensor boost mode. What are the coordinates of the distress signal?
+
  */
 
 import Foundation
@@ -66,6 +76,8 @@ struct Puzzle09: Puzzle {
     }
 
     func part2() -> String {
+        var inputt = input
+        computer.compute(program: &inputt, printOutputs: true, inputs: 2)
         return ""
     }
 }
