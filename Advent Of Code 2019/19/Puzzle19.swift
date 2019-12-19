ad9bdb041ecda4880102d059f4fafdf255822086
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2019-12-19 16:24:34 +0530

    Solution for 19_02

--- Advent Of Code 2019/19/Puzzle19.swift
+++ Advent Of Code 2019/19/Puzzle19.swift
@@ -36,11 +36,63 @@
 
  However, you'll need to scan a larger area to understand the shape of the beam. How many points are affected by the tractor beam in the 50x50 area closest to the emitter? (For each of X and Y, this will be 0 through 49.)
 
+ --- Part Two ---
+
+ You aren't sure how large Santa's ship is. You aren't even sure if you'll need to use this thing on Santa's ship, but it doesn't hurt to be prepared. You figure Santa's ship might fit in a 100x100 square.
+
+ The beam gets wider as it travels away from the emitter; you'll need to be a minimum distance away to fit a square of that size into the beam fully. (Don't rotate the square; it should be aligned to the same axes as the drone grid.)
+
+ For example, suppose you have the following tractor beam readings:
+
+ #.......................................
+ .#......................................
+ ..##....................................
+ ...###..................................
+ ....###.................................
+ .....####...............................
+ ......#####.............................
+ ......######............................
+ .......#######..........................
+ ........########........................
+ .........#########......................
+ ..........#########.....................
+ ...........##########...................
+ ...........############.................
+ ............############................
+ .............#############..............
+ ..............##############............
+ ...............###############..........
+ ................###############.........
+ ................#################.......
+ .................########OOOOOOOOOO.....
+ ..................#######OOOOOOOOOO#....
+ ...................######OOOOOOOOOO###..
+ ....................#####OOOOOOOOOO#####
+ .....................####OOOOOOOOOO#####
+ .....................####OOOOOOOOOO#####
+ ......................###OOOOOOOOOO#####
+ .......................##OOOOOOOOOO#####
+ ........................#OOOOOOOOOO#####
+ .........................OOOOOOOOOO#####
+ ..........................##############
+ ..........................##############
+ ...........................#############
+ ............................############
+ .............................###########
+
+ In this example, the 10x10 square closest to the emitter that fits entirely within the tractor beam has been marked O. Within it, the point closest to the emitter (the only highlighted O) is at X=25, Y=20.
+
+ Find the 100x100 square closest to the emitter that fits entirely within the tractor beam; within that square, find the point closest to the emitter. What value do you get if you take that point's X coordinate, multiply it by 10000, then add the point's Y coordinate? (In the example above, this would be 250020.)
+
  */
 
 import Foundation
 
-fileprivate class Position {
+fileprivate class Position: CustomStringConvertible {
+    var description: String {
+        "(\(x) \(y) \(character))"
+    }
+
     let x, y: Int
     var character: Character
 
@@ -62,14 +114,9 @@ struct Puzzle19: Puzzle {
         input = InputFileReader.readInput(id: "19", separator: ",").map { Int($0.trimmingCharacters(in: .whitespacesAndNewlines))! }
     }
 
-    private func plot(_ positions: [Position]) {
-        let canvas = Canvas(pixels: positions.map { $0.toPixel() }, anchor: .topLeft)
-        canvas.render()
-    }
-
-    func part1() -> String {
-        let positions = (0..<50).flatMap { (y) -> [Position] in
-            (0..<50).map { (x) -> Position in
+    private func map(size: Int) -> [Position] {
+        let positions = (0..<size).flatMap { (y) -> [Position] in
+            (0..<size).map { (x) -> Position in
                 Position(x: x, y: y)
             }
         }
@@ -83,11 +130,43 @@ struct Puzzle19: Puzzle {
             }
         }
 
-        plot(positions)
-        return "\(positions.filter { $0.character == "#" }.count)"
+        return positions
     }
 
+    private func plot(_ positions: [Position]) {
+        let canvas = Canvas(pixels: positions.map { $0.toPixel() }, anchor: .topLeft)
+        canvas.render()
+    }
+
+    func part1() -> String {
+        let m = map(size: 50)
+        plot(m)
+        return "\(m.filter { $0.character == "#" }.count)"
+    }
+
+    // Takes around 10 mins to run. Majority of time is spent in generating the 1200x1200 map by intcode.
+    // Arrived at 1200 value arbitrarily. The answer is (424, 964)
     func part2() -> String {
-        return ""
+        let mapSize = 1200
+        let shipSize = 100
+        let m = map(size: mapSize).chunked(into: mapSize)
+
+        for row in m {
+            if let rowlst = row.lastIndex(where: { $0.character == "#" }) {
+                let rowfst = rowlst - shipSize + 1
+                if row.indices.contains(rowfst) {
+                    let colfst = row[rowfst].y
+                    let collst = colfst + shipSize - 1
+                    if m.indices.contains(collst) {
+                        if m[collst][rowfst].character == "#" {
+                            return "\(rowfst), \(colfst)"
+                        }
+                    } else {
+                        break
+                    }
+                }
+            }
+        }
+        fatalError("Square of specified size not found within tractor beam!!!")
     }
 }
--- Advent Of Code 2019/Helpers/Extensions.swift
+++ Advent Of Code 2019/Helpers/Extensions.swift
@@ -15,6 +15,12 @@ extension Array {
             return self[index % self.count]
         }
     }
+
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0..<Swift.min($0 + size, count)])
+        }
+    }
 }
 
 extension BinaryInteger {
